b48fd7d8bc29e4a20a9f39049253c379c4cbf664
florian <blog.florian.luescher@gmail.com>
2025-07-21 19:50:55 +0200

    enhanced backend with additional APIs: /nodes and /node_classes

--- app/backend/main.py
+++ app/backend/main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv, find_dotenv
 
 from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.params import Query
 from neo4j import GraphDatabase
 app = FastAPI()
 
@@ -33,6 +34,29 @@ def run_cypher_query(query: str):
     with driver.session() as session:
         result = session.run(query)
         return [dict(record) for record in result]
+
+@app.get("/node_classes")
+def get_classes():
+    try:
+        data = run_cypher_query("""
+        MATCH (n) RETURN DISTINCT n.class
+        """)
+        return {"success": True, "data": data}
+    except Exception as e:
+        return {"success": False, "error": str(e)}
+
+@app.get("/nodes")
+def query_cypher(node_class: str = Query(..., min_length=1)):
+    if not node_class.isidentifier() or node_class == "":
+        return {"error": "Invalid label name"}
+    try:
+        data = run_cypher_query(f"""
+        MATCH (n {{class: "{node_class}"}}) 
+        RETURN n
+        """)
+        return {"success": True, "data": data}
+    except Exception as e:
+        return {"success": False, "error": str(e)}
 
 @app.post("/query")
 def query_cypher(query: str = Form(...)):
